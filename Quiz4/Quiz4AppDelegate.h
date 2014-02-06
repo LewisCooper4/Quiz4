b982c75714de0b693074ab2c612b6f6a3cacccf2
Matthew Guidon <pgls@Matthew-Guidons-MacBook-Pro.local>
2014-02-06 17:08:12 -0500

    Initial Commit

---
+++ Quiz4/Quiz4AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  Quiz4AppDelegate.h
+//  Quiz4
+//
+//  Created by Matthew Guidon on 2/6/14.
+//  Copyright (c) 2014 Matthew Guidon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Quiz4AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
